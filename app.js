2304264ec9245f0e88b969c9a6dcafdb676b72cb
Dinuka Sanjana Madhumal <98506454+DinukaSanjana@users.noreply.github.com>
2025-11-12 06:52:32 +0530

    Update database connection details in app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const port = 3000;
 
 // Create connection to MySQL
 const db = mysql.createConnection({
-    host: 'testdb-1.c34egoce037f.ap-south-1.rds.amazonaws.com',
-    user: 'root',
+    host: 'testdb-1.cnso6k62wnhp.ap-south-1.rds.amazonaws.com',
+    user: 'admin',
     password: '12345678',
     database: 'testdb_1'
 });
